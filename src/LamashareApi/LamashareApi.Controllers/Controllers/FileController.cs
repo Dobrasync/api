d80afc1e92c2e789e92081541403646dd38470bd
OminousLama <149961466+OminousLama@users.noreply.github.com>
2024-08-11 19:30:18 +0200

    fix: Fixing swagger ui not loading

--- src/LamashareApi/LamashareApi.Controllers/Controllers/FileController.cs
+++ src/LamashareApi/LamashareApi.Controllers/Controllers/FileController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LamashareApi.Controllers;
 
-[Microsoft.AspNetCore.Components.Route("files")]
+[Route("files")]
 public class FileController : BaseController
 {
 }
